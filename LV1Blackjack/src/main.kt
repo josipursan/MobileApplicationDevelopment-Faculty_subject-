d25cbff38534a778b732f7d31d9e13f692e05117
unknown <josip.ursan@gmail.com>
2020-05-01 11:05:52 +0200

    Third commit - major code revision

--- LV1Blackjack/src/main.kt
+++ LV1Blackjack/src/main.kt
@@ -1,108 +1,141 @@
-import java.util.Scanner
-
-fun main(args:Array<String>)
+fun main()
 {
-    val reader = Scanner(System.`in`)
+    var playerHuman_cardSum = 0
+    var playerComputer_cardSum = 0
 
-    var cardDeck = mutableMapOf<String, Int>("Ace" to 4, "Two" to 4, "Three" to 0, "Four" to 0, "Five" to 0, "Six" to 0, "Seven" to 0, "Eight" to 0,
-            "Nine" to 0, "Ten" to 0, "Jack" to 4, "Queen" to 4, "King" to 0)
+    var positiveInput_upperCase = 'Y'
+    var positiveInput_lowerCase = 'y'
+    val computerNewCardChoices = listOf<String>("y", "n")
 
-    val newDealer = Dealer()
-    println("Do you want to play? (y/n)")
-    val correctInput : Char = 'y'
-    val correctInputUpper : Char = 'Y'
-    var playYesNo : String? = readLine()
+    var newDealer = dealer()
 
-    while(playYesNo == correctInput.toString() || playYesNo == correctInputUpper.toString())
+    var newPlayer_human = player()
+    var newPlayer_computer = player()
+
+    do
     {
-        newDealer.dealCard(cardDeck)
-        var dealerSum = newDealer.getDealerCardSum()
-        var playerSum = newDealer.getPlayerCardSum()
+        if(newPlayer_human.getCardSum() == 0 && newPlayer_computer.getCardSum() == 0)   //INITIAL DEAL
+        {
+            newPlayer_human.updateCardSum(newDealer.dealAnotherCard())
+            playerHuman_cardSum = newPlayer_human.getCardSum()
+            println("Human card sum : $playerHuman_cardSum")
 
-        if(dealerSum >= 22)
+            newPlayer_computer.updateCardSum(newDealer.dealAnotherCard())
+            playerComputer_cardSum = newPlayer_computer.getCardSum()
+        }
+        else
         {
-            println("Dealer loses")
+            newPlayer_human.updateCardSum(newDealer.dealAnotherCard())
+            playerHuman_cardSum = newPlayer_human.getCardSum()
+            println("Human card sum : $playerHuman_cardSum")
+
+            var randomComputerChoice = computerNewCardChoices.random()
+            if(newPlayer_computer.getCardSum() >= 17)
+            {
+                randomComputerChoice = computerNewCardChoices[1]
+            }
+            if(randomComputerChoice == computerNewCardChoices[0])
+            {
+                var newCardValue = newDealer.dealAnotherCard()
+                if(newCardValue == 11 && ((newPlayer_computer.getCardSum() + newCardValue)>21))
+                {
+                    newCardValue = 1
+                }
+                newPlayer_computer.updateCardSum(newCardValue)
+                playerComputer_cardSum = newPlayer_computer.getCardSum()
+            }
+            else
+            {
+                println("Computer didn't draw another card")
+            }
+        }
+
+        if(playerHuman_cardSum > 21)
+        {
+            println("Player sum : $playerHuman_cardSum.\tComputer sum : $playerComputer_cardSum\nPlayer loses.")
             break
         }
-        else if(dealerSum == 21)
+        else if(playerComputer_cardSum > 21)
         {
-            println("Dealer wins")
+            println("Player sum : $playerHuman_cardSum.\tComputer sum : $playerComputer_cardSum\nComputer loses.")
             break
         }
-        else if(playerSum >= 22)
+        else if(playerHuman_cardSum == 21 && playerComputer_cardSum < 21)
         {
-            println("Player loses")
+            println("Player sum : $playerHuman_cardSum.\tComputer sum : $playerComputer_cardSum\tComputer loses, human wins.")
             break
         }
-        else if(playerSum == 21)
+        else if(playerComputer_cardSum == 21 && playerHuman_cardSum < 21)
         {
-            println("Player wins")
+            println("Player sum : $playerHuman_cardSum.\tComputer sum : $playerComputer_cardSum\tPlayer loses, computer wins.")
+            break
+        }
+        else if (playerComputer_cardSum == 21 && playerHuman_cardSum == 21) {
+            println("Player sum : $playerHuman_cardSum.\tComputer sum : $playerComputer_cardSum\tIt's a draw!")
             break
         }
 
-        println("Another card? (y/n)")
-        playYesNo = readLine()
+        print("Do you want another card? : ")
+        val userChoice = readLine()!!
+    }while (userChoice == positiveInput_lowerCase.toString() || userChoice == positiveInput_upperCase.toString())
 
-    }
+    println("Player sum : $playerHuman_cardSum \t Computer sum : $playerComputer_cardSum \n")
 }
 
 
 
-class Dealer
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+class player
 {
-    private var dealerCardSum : Int = 0
-    private var playerCardSum : Int = 0
+    private var cardSum = 0
 
-    public fun getDealerCardSum():Int
+    fun getCardSum(): Int
     {
-        println("Current dealer sum is $dealerCardSum")
-        return dealerCardSum
+        return cardSum
     }
-
-    public fun getPlayerCardSum():Int
+    fun updateCardSum(newCardValue : Int)
     {
-        println("Current player sum is $playerCardSum")
-        return playerCardSum
+        cardSum += newCardValue
     }
+}
 
-    public fun dealCard(mapCardDeck: MutableMap<String, Int>)
+class dealer
+{
+    var cardDeck = mutableMapOf<String, Int>("Ace" to 4, "Two" to 4, "Three" to 4, "Four" to 4, "Five" to 4, "Six" to 4, "Seven" to 4, "Eight" to 4,
+        "Nine" to 4, "Ten" to 4, "Jack" to 4, "Queen" to 4, "King" to 4)
+    val cardList = listOf("Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King")
+    val cardStrengths = mapOf("Jack" to 10, "Queen" to 10, "King" to 10, "Ace" to 11, "Two" to 2, "Three" to 3, "Four" to 4, "Five" to 5, "Six" to 6, "Seven" to 7,
+        "Eight" to 8, "Nine" to 9, "Ten" to 10)
+
+    private fun generateRandomCard(): String
     {
-        val cardList = listOf("Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King")
-        val cardStrengths = mapOf("Jack" to 10, "Queen" to 10, "King" to 10, "Ace" to 1, "Two" to 2, "Three" to 3, "Four" to 4,
-                "Five" to 5, "Six" to 6, "Seven" to 7, "Eight" to 8, "Nine" to 9, "Ten" to 10)
+        var randomCard = cardList.random()
 
-        var decrementCardAmount = 0
-
-        var randomCard : String = cardList.random()
-
-        while(mapCardDeck[randomCard] == 0)
+        while(cardDeck[randomCard] == 0)
         {
             randomCard = cardList.random()
-            if(mapCardDeck[randomCard] != 0)
+            if(cardDeck[randomCard] != 0)
             {
-                if(dealerCardSum >= 17)
-                {
-                    continue
-                }
-                else
-                {
-                    var decrementCardAmount = mapCardDeck.getOrDefault(randomCard, 0) - 1
-                    mapCardDeck.put(randomCard, decrementCardAmount)
-                    dealerCardSum += cardStrengths[randomCard]!!
-                }
+                break
             }
         }
+        return randomCard
+    }
 
-        randomCard = cardList.random()
-        while(mapCardDeck[randomCard] == 0) {
-            randomCard = cardList.random()
+    private fun decrementAmountOfCardsInDeck(cardName : String)
+    {
+        var decrementedCardAmount = cardDeck.getOrDefault(cardName, 0) - 1
+        cardDeck.put(cardName, decrementedCardAmount)
+    }
 
-            if (mapCardDeck[randomCard] != 0)
-            {
-                decrementCardAmount = mapCardDeck.getOrDefault(randomCard, 0) - 1
-                mapCardDeck.put(randomCard, decrementCardAmount)
-                playerCardSum += cardStrengths[randomCard]!!
-            }
-        }
+    fun dealAnotherCard(): Int
+    {
+        var randomCard = generateRandomCard()
+        decrementAmountOfCardsInDeck(randomCard)
+        return cardStrengths[randomCard]!!
     }
 }
